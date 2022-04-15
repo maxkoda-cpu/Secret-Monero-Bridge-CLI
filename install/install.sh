51432a03b0d8b3d09d50705282b6d37532a862d2
maxkoda-cpu <59777196+maxkoda-cpu@users.noreply.github.com>
2022-04-15 14:14:09 -0500

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#Download secretcli version 1.2.2...
+wget https://github.com/scrtlabs/SecretNetwork/releases/download/v1.2.2/secretcli-Linux
+
+mv secretcli-Linux secretcli
+chmod +x secretcli
+mv secretcli /usr/bin/
+secretcli version
+
+
+#Download smbridge-linux...
+wget https://github.com/maxkoda-cpu/Secret-Monero-Bridge-CLI/releases/download/v1.0-beta2/smbridge-linux
+chmod +x smbridge-linux
+mv smbridge-linux /usr/bin/
+smbridge-linux --version
+
+
+echo Enter datahub AUTH KEY:
+read auth
+echo $auth
+
+str='s/<AUTH_CODE>/'$auth'/'
+#echo $str
+
+sed -i $str caddy-install.sh
+chmod +x caddy-install.sh
+./caddy-install.sh
