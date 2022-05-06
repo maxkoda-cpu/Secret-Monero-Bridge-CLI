cc0e383a1a51dccc24f83dead7d1494f305a3fc8
maxkoda-cpu <59777196+maxkoda-cpu@users.noreply.github.com>
2022-05-06 13:05:37 -0500

    Create install.sh

---
+++ install-macOS/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# Secret Monero Bridge CLI - macOS install script
+#DEPENDENCIES:
+# 1. Install Homebrew
+# 2. Install wget
+# See the README.md file
+
+#Download secretcli version 1.2.2...
+wget https://github.com/scrtlabs/SecretNetwork/releases/download/v1.2.2/secretcli-macOS
+
+mv secretcli-macOS secretcli
+chmod +x secretcli
+mv secretcli /usr/local/bin/
+secretcli version
+
+
+#Download smbridge-macos...
+wget https://github.com/maxkoda-cpu/Secret-Monero-Bridge-CLI/releases/download/v1.0-beta3/smbridge-macos
+chmod +x smbridge-macos
+mv smbridge-macos /usr/local/bin/
+smbridge-macos --version
+
+brew install caddy
+
+echo Enter datahub AUTH KEY:
+read auth
+echo $auth
+
+str='s/<AUTH_CODE>/'$auth'/'
+#echo $str
+
+sed -i '' $str Caddyfile
